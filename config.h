0df3a9cf40aa170596c457c61562482802a032c1
rishabh181104 <rishabhhaldiya18@gmail.com>
2024-09-06 04:56:12 +0530

    just some changes

--- config.h
+++ config.h
@@ -16,7 +16,7 @@ static const int topbar                       = 1;   /* 0 means bottom bar */
 #define ICONSIZE                              17    /* icon size */
 #define ICONSPACING                           5     /* space between icon and title */
 #define SHOWWINICON                           1     /* 0 means no winicon */
-static const char *fonts[]                    = { "MesloLGS Nerd Font Mono:size=14", "NotoColorEmoji:pixelsize=16:antialias=true:autohint=true" };
+static const char *fonts[]                    = { "MesloLGS Nerd Font Mono:size=12", "NotoColorEmoji:pixelsize=14:antialias=true:autohint=true" };
 static const char normbordercolor[]           = "#3B4252";
 static const char normbgcolor[]               = "#2E3440";
 static const char normfgcolor[]               = "#D8DEE9";
