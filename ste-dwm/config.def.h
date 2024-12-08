defa514f65797f4ae164f7c1176dedce86d2cd4a
Rishabh Haldiya <rishabhhaldiya18@gmail.com>
2024-12-08 18:13:24 +0530

    some changes

--- ste-dwm/config.def.h
+++ ste-dwm/config.def.h
@@ -155,6 +155,7 @@ static const Key keys[] = {
 
     { MODKEY,                           XK_space,   spawn,          SHCMD("rofi -show drun") },
     { MODKEY,                           XK_Return,  spawn,          SHCMD("alacritty")},
+    { MODKEY,                           XK_g,       spawn,          SHCMD("github-desktop")},
     { MODKEY,                           XK_b,       spawn,          SHCMD("brave-browser-nightly")},
     { MODKEY,                           XK_o,       spawn,          SHCMD("zeditor")},
     { MODKEY,                           XK_v,       spawn,          SHCMD("obsidian")},
